323def45cece032b626dfd7a99f219bcb10978c9
Dmitry Fyodorov <df@euro-motor.ru>
2015-12-19 17:51:40 +0300

    head src

---
+++ smartclient_head.js
@@ -0,0 +1,21 @@
+<HTML><HEAD>
+ <SCRIPT>var isomorphicDir="http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/";</SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_Core.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_Foundation.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_Containers.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_Grids.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_Forms.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/system/modules/ISC_DataBinding.js></SCRIPT>
+ <SCRIPT SRC=http://www.smartclient.com/v/MAIN_2015-12-17_1450424649_alex_apex/isomorphic/skins/SmartClient/load_skin.js></SCRIPT>
+ </HEAD><BODY>
+ 
+ <SCRIPT>
+ isc.Button.create({
+     title:"Button",
+     click:"isc.say('Hello World')"
+ });
+ </SCRIPT>
+ 
+ </BODY>
+ </HTML>
+ 
